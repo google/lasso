503e4f53e38fcfef8bf879abdee84c30bc0c7196
Ayman Farhat <aymanf@google.com>
2020-08-31 14:18:26 +0400

    Fix for issue #1

--- src/utils/api.js
+++ src/utils/api.js
@@ -81,8 +81,25 @@ function objectFromBuffer(objectData) {
   return parsedObj;
 }
 
+/**
+ * @param {String} str
+ * @return {Boolean}
+ */
+function isURL(str) {
+  const pattern = new RegExp('^(https?:\\/\\/)?'+
+  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+
+  '((\\d{1,3}\\.){3}\\d{1,3}))'+
+  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+  '(\\?[;&a-z\\d%_.~+=-]*)?'+
+  '(\\#[-a-z\\d_]*)?$','i');
+
+  return pattern.test(str);
+}
+
 module.exports = {
   getChunkedList,
   validateAuditRequest,
   objectFromBuffer,
+  isURL,
 };
+

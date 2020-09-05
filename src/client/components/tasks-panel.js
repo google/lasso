f5280fb83c552ba72cc3dacbbb7ebe78c4fb0fcd
Ayman Farhat <aymanf@google.com>
2020-09-05 10:57:44 +0400

    Display empty queue message

--- src/client/components/tasks-panel.js
+++ src/client/components/tasks-panel.js
@@ -45,6 +45,41 @@ class TasksPanel extends Component {
   }
 
   /**
+   * renderTaskRows
+   */
+  renderTaskRows() {
+    return this.state.tasksInView.map((task) => {
+      return html`
+          <tr>
+            <td>${task.name}</td>
+            <td>${task.dispatchCount}</td>
+            <td>${task.responseCount}</td>
+            <td>${task.firstAttempt}</td>
+            <td>${task.lastAttempt}</td>
+          </tr>`;
+    });
+  }
+
+  renderTable() {
+    return html`
+      <table>
+        <thead>
+          <tr>
+            <th>Task name</th>
+            <th>Dispatch count</th>
+            <th>Response count</th>
+            <th>First attempt</th>
+            <th>Last attempt</th>
+          </tr>
+        </thead>
+        <tbody>
+        ${this.renderTaskRows()}
+        </tbody>
+      </table>
+    `
+  }
+
+  /**
    * Render
    * @param {Object} props
    * @param {Object} state
@@ -53,35 +88,13 @@ class TasksPanel extends Component {
   render(props, state) {
     return html`
       <main class="c-output">
-                <div class="toolbar">
-                  Active Audit Tasks
-                </div>
-                <div class="panel-content">
-                  <table>
-                    <thead>
-                      <tr>
-                        <th>Task name</th>
-                        <th>Dispatch count</th>
-                        <th>Response count</th>
-                        <th>First attempt</th>
-                        <th>Last attempt</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                    ${state.tasksInView.map((task) => {
-                      return html`
-                      <tr>
-                        <td>${task.name}</td>
-                        <td>${task.dispatchCount}</td>
-                        <td>${task.responseCount}</td>
-                        <td>${task.firstAttempt}</td>
-                        <td>${task.lastAttempt}</td>
-                      </tr>`;
-                    })}
-                    </tbody>
-                  </table>
-                </div>
-              </main>
+         <div class="toolbar">
+           Active Audit Tasks
+         </div>
+         <div class="panel-content">
+           ${(this.state.tasksInView.length > 0) ? this.renderTable() : 'No tasks in queue'}
+         </div>
+       </main>
         `;
   }
 }
